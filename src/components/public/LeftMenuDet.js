343983c01332018cc6aa83813287385e4ffde342
liuhao <liuhao@etoc.cn>
2018-01-31 17:51:34 +0800

    菜单修改

--- src/components/public/LeftMenuDet.js
+++ src/components/public/LeftMenuDet.js
@@ -18,7 +18,7 @@ class LeftMenuDet extends React.Component {
           <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
         </Button>
         <Menu
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['5']}
           defaultOpenKeys={['sub1']}
           mode="inline"
           theme="dark"
@@ -26,29 +26,12 @@ class LeftMenuDet extends React.Component {
         >
           <Menu.Item key="1">
             <Icon type="pie-chart" />
-            <span>Option 1</span>
+            <span>统计管理</span>
           </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="inbox" />
-            <span>Option 3</span>
-          </Menu.Item>
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
+          <SubMenu key="sub1" title={<span><Icon type="desktop" /><span>系统管理</span></span>}>
+            <Menu.Item key="5">用户管理</Menu.Item>
+            <Menu.Item key="6">角色管理</Menu.Item>
+            <Menu.Item key="7">系统日志</Menu.Item>
           </SubMenu>
         </Menu>
       </div>
