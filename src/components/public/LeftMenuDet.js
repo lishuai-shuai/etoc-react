58733850db0a29aaa8c3e6865c54bcfbed978183
liuhao <liuhao@etoc.cn>
2018-01-31 17:18:18 +0800

    菜单修改

--- src/components/public/LeftMenuDet.js
+++ src/components/public/LeftMenuDet.js
@@ -1,66 +1,61 @@
 import React from 'react';
-import { Menu, Icon, Switch } from 'antd';
+import { Menu, Icon, Button } from 'antd';
 const SubMenu = Menu.SubMenu;
 
 class LeftMenuDet extends React.Component {
   state = {
-    theme: 'dark',
-    current: '1',
+    collapsed: false,
   }
-  changeTheme = (value) => {
+  toggleCollapsed = () => {
     this.setState({
-      theme: value ? 'dark' : 'light',
-    });
-  }
-  handleClick = (e) => {
-    console.log('click ', e);
-    this.setState({
-      current: e.key,
+      collapsed: !this.state.collapsed,
     });
   }
   render() {
     return (
-      <div>
-        <Switch
-          checked={this.state.theme === 'dark'}
-          onChange={this.changeTheme}
-          checkedChildren="Dark"
-          unCheckedChildren="Light"
-        />
-        <br />
-        <br />
+      <div style={{ width: 256 }}>
+        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+          <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+        </Button>
         <Menu
-          theme={this.state.theme}
-          onClick={this.handleClick}
-          style={{ width: 256 }}
+          defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
-          selectedKeys={[this.state.current]}
           mode="inline"
+          theme="dark"
+          inlineCollapsed={this.state.collapsed}
         >
+          <Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <span>Option 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="desktop" />
+            <span>Option 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="inbox" />
+            <span>Option 3</span>
+          </Menu.Item>
           <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigtion Two</span></span>}>
             <Menu.Item key="5">Option 5</Menu.Item>
             <Menu.Item key="6">Option 6</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="7">Option 7</Menu.Item>
-              <Menu.Item key="8">Option 8</Menu.Item>
-            </SubMenu>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
           </SubMenu>
-          <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
             <Menu.Item key="9">Option 9</Menu.Item>
             <Menu.Item key="10">Option 10</Menu.Item>
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
           </SubMenu>
         </Menu>
       </div>
     );
   }
 }
+
+
 
 export default LeftMenuDet;
