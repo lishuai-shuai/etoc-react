b6b2b4dfb2019d4017565c21a5d8b591e621f9a5
liuhao <liuhao@etoc.cn>
2018-01-31 17:06:32 +0800

    1

---
+++ src/routes/public/LeftMenu.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { connect } from 'dva';
+import LeftMenuDet from '../../components/public/LeftMenuDet';
+
+const LeftMenu = (props) => (
+  <LeftMenuDet></LeftMenuDet>
+);
+export default LeftMenu;
