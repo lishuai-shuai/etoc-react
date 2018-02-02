99b9a953a1f67159cdc6cd48dbaf2b1046571a0c
liuhao <liuhao@etoc.cn>
2018-02-02 16:21:13 +0800

    1

--- src/components/system/RoleSetDet.js
+++ src/components/system/RoleSetDet.js
@@ -7,25 +7,56 @@ const { Column, ColumnGroup } = Table;
 
 
 const { Header, Content } = Layout;
-const data = [{
+const data = [
+{
   key: '1',
-  firstName: 'John',
-  lastName: 'Brown',
+  name: '李帅',
+  mail:'lis@163.com',
   age: 32,
-  address: 'New York No. 1 Lake Park',
-}, {
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省武汉市',
+}, 
+{
   key: '2',
-  firstName: 'Jim',
-  lastName: 'Green',
+  name: '李帅帅',
+  mail:'lis@163.com',
   age: 42,
-  address: 'London No. 1 Lake Park',
-}, {
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省荆州市',
+}, 
+{
   key: '3',
-  firstName: 'Joe',
-  lastName: 'Black',
+  name: '李帅帅帅',
   age: 32,
-  address: 'Sidney No. 1 Lake Park',
-}];
+  mail:'lis@163.com',
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省荆州市',
+},
+{
+  key: '4',
+  name: '李帅',
+  mail:'lis@163.com',
+  age: 32,
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省武汉市',
+}, 
+{
+  key: '5',
+  name: '李帅帅',
+  mail:'lis@163.com',
+  age: 42,
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省荆州市',
+}, 
+{
+  key: '6',
+  name: '李帅帅帅',
+  age: 32,
+  mail:'lis@163.com',
+  lastLogin:'2018-02-02	18:00:00',
+  address: '湖北省荆州市',
+}
+];
 
 
 function RoleSetDet() {
@@ -37,40 +68,39 @@ function RoleSetDet() {
 		<div  style={{margin:'0 0 0 200px'}}>
 			<Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 700 }}>
 			  <Table dataSource={data}>
-				<ColumnGroup title="Name">
-				  <Column
-					title="First Name"
-					dataIndex="firstName"
-					key="firstName"
-				  />
-				  <Column
-					title="Last Name"
-					dataIndex="lastName"
-					key="lastName"
-				  />
-				</ColumnGroup>
+				<Column
+				  title="用户名"
+				  dataIndex="name"
+				  key="name"
+				/>
 				<Column
 				  title="Age"
 				  dataIndex="age"
 				  key="age"
 				/>
 				<Column
-				  title="Address"
+				  title="邮箱"
+				  dataIndex="mail"
+				  key="mail"
+				/>
+				<Column
+				  title="用户地址"
 				  dataIndex="address"
 				  key="address"
 				/>
 				<Column
-				  title="Action"
+				  title="最后登录"
+				  dataIndex="lastLogin"
+				  key="lastLogin"
+				/>
+				<Column
+				  title="操作"
 				  key="action"
 				  render={(text, record) => (
 					<span>
-					  <a href="#">Action һ {record.name}</a>
-					  <Divider type="vertical" />
-					  <a href="#">Delete</a>
-					  <Divider type="vertical" />
-					  <a href="#" className="ant-dropdown-link">
-						More actions <Icon type="down" />
-					  </a>
+					  <a href="#" class="pub-a pub-a-sml blue-a"><Icon type="eye-o" />查看</a>
+					  <a href="#" class="pub-a pub-a-sml green-a ml10 mr10"><Icon type="form" />编辑</a>
+					  <a href="#" class="pub-a pub-a-sml red-a"><Icon type="delete" />删除</a>
 					</span>
 				  )}
 				/>
